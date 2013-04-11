bb6cb167d4e8e33499bdcb21723c6a7551c5319d
Austin Benson <arbenson@gmail.com>
2013-04-10 20:02:53 -0700

    clean up the main file

--- src/tt.cpp
+++ src/tt.cpp
@@ -3,15 +3,27 @@
 
 using namespace std;
 
+#define CLOCK_DIFF_SECS(ck1, ck0) (double(ck1-ck0) / CLOCKS_PER_SEC)
+
 int optionsCreate(int argc, char** argv, map<string,string>& options)
 {
   //iA(argc%2==1);
   options.clear();
   //2. get extra data
   for(int k=1; k<argc; k=k+2) {
-	options[ string(argv[k]) ] = string(argv[k+1]);
+    options[ string(argv[k]) ] = string(argv[k+1]);
   }
   return 0;
+}
+
+string findOption(map<string,string>& opts, string option)
+{
+  map<string,string>::iterator mi = opts.find(option);
+  if (mi == opts.end()) {
+    cout << "Missing option " << option << endl;
+    return "";
+  }
+  return mi->second;
 }
 
 int main(int argc, char** argv)
@@ -19,6 +31,7 @@ int main(int argc, char** argv)
   MPI_Init(&argc, &argv);
   int mpirank;  MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
   int mpisize;  MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
+
   //0. init and get options
   srand48(time(NULL));  iA(argc%1==0);
   clock_t ck0, ck1;
@@ -26,10 +39,16 @@ int main(int argc, char** argv)
   vector<int> all(1,1);
   map<string,string> opts;  optionsCreate(argc, argv, opts);
   map<string,string>::iterator mi;
+  string opt;
+
   //1. read data
   ParVec<int, Point3, PtPrtn> pos;
-  mi = opts.find("-posfile");  assert(mi!=opts.end());
-  istringstream piss;  iC( Separate_Read((*mi).second, piss) );
+  opt = findOption(opts, "-posfile");
+  if (opt.empty()) {
+    return 0;
+  }
+
+  istringstream piss;  iC( Separate_Read(opt, piss) );
   iC( deserialize(pos, piss, all) );
   vector<int>& tmpinfo = pos.prtn().ownerinfo();
   int numpts = tmpinfo[tmpinfo.size()-1]; //LEXING: THIS CONTAINS THE TOTAL NUMBER OF POINTS
@@ -37,8 +56,13 @@ int main(int argc, char** argv)
     cout<<"Done reading pos "<<pos.lclmap().size()<<" "<<pos.prtn().ownerinfo().size()<<endl;
   }
   ParVec<int, cpx, PtPrtn> den;
-  mi = opts.find("-denfile");  assert(mi!=opts.end());
-  istringstream diss; iC( Separate_Read((*mi).second, diss) );
+
+  opt = findOption(opts, "-denfile");
+  if (opt.empty()) {
+    return 0;
+  }
+
+  istringstream diss; iC( Separate_Read(opt, diss) );
   iC( deserialize(den, diss, all) );
   if(mpirank==0) {
     cout<<"Done reading den "<<den.lclmap().size()<<" "<<den.prtn().ownerinfo().size()<<endl;
@@ -51,9 +75,10 @@ int main(int argc, char** argv)
   Kernel3d knl(KNL_HELM);
   mi = opts.find("-knl");
   if(mi!=opts.end()) {
-	istringstream ss((*mi).second);
-	int type;	ss>>type;
-	knl.type() = type;
+    istringstream ss(mi->second);
+    int type;
+    ss >> type;
+    knl.type() = type;
   }
   Mlib3d mlib("mlib3d_");
   mlib.knl() = knl;
@@ -63,8 +88,12 @@ int main(int argc, char** argv)
     cout<<"Done reading mlib "<<mlib.w2ldmap().size()<<" "<<mlib.w2hdmap().size()<<endl;
   }
   IntNumTns geomprtn;
-  mi = opts.find("-geomprtn");  assert(mi!=opts.end());
-  istringstream giss;  iC( Shared_Read((*mi).second, giss) );
+  opt = findOption(opts, "-geomprtn");
+  if (opt.empty()) {
+    return 0;
+  }
+  
+  istringstream giss;  iC( Shared_Read(mi->second, giss) );
   iC( deserialize(geomprtn, giss, all) );
   if(mpirank==0) {
     cout<<"Done reading geomprtn "<<geomprtn.m()<<" "<<geomprtn.n()<<" "<<geomprtn.p()<<endl;
@@ -80,44 +109,58 @@ int main(int argc, char** argv)
   wave.K() = 64;
   wave.ctr() = Point3(0,0,0);
   wave.ptsmax() = 100; //LY: check
+
   //2. setup
   t0 = time(0);
   iC( wave.setup(opts) );
   t1 = time(0);
   if(mpirank==0) {	cout<<"wave setup used "<<difftime(t1,t0)<<"secs "<<endl;  }
   double time_setup = difftime(t1,t0);
+
   //3. eval
   double time_eval;
-  for(int i=0; i<1; i++) {
-	t0 = time(0);
-	iC( wave.eval(den, val) );
-	t1 = time(0);
-	if(mpirank==0) {	cout<<"wave eval used "<<difftime(t1,t0)<<"secs "<<endl;  }
-	time_eval = difftime(t1,t0);
+  t0 = time(0);
+  iC( wave.eval(den, val) );
+  t1 = time(0);
+  if(mpirank==0) {
+    cout<<"wave eval used "<<difftime(t1,t0)<<"secs "<<endl;
   }
+  time_eval = difftime(t1,t0);
+
   ostringstream oss; iC( serialize(val, oss, all) );
-  mi = opts.find("-valfile");  assert(mi!=opts.end());
-  iC( Separate_Write((*mi).second, oss) );
+
+  opt = findOption(opts, "-valfile");
+  if (opt.empty()) {
+    return 0;
+  }
+  iC( Separate_Write(opt, oss) );
+
   //4. check
   IntNumVec chkkeyvec;
-  mi = opts.find("-chkfile");  assert(mi!=opts.end());
-  istringstream iss;  iC( Shared_Read((*mi).second, iss) );
+  opt = findOption(opts, "-chkfile");
+  if (opt.empty()) {
+    return 0;
+  }
+  istringstream iss;  iC( Shared_Read(opt, iss) );
   iC( deserialize(chkkeyvec, iss, all) );
   int numchk = chkkeyvec.m();
   double relerr;
   ck0 = clock();
   iC( wave.check(den, val, chkkeyvec, relerr) );
   ck1 = clock();
-  if(mpirank==0) {	 cout<<"wave check used "<<double(ck1-ck0)/CLOCKS_PER_SEC<<"secs "<<endl; }
-  //
-  double time_drct = (double(ck1-ck0)/CLOCKS_PER_SEC) * numpts / double(numchk);
+  if(mpirank==0) {
+    cout<<"wave check used "<< CLOCK_DIFF_SECS(ck1, ck0) << "secs "<<endl;
+  }
+
+  //5. output results
+  double time_drct = CLOCK_DIFF_SECS(ck1, ck0) * numpts / double(numchk);
   if(mpirank==0) {
 	printf("----------------------\n");
 	printf("RESULT\n");
 	printf("K  %.2e\n", wave.K());
 	printf("Ta %.2e\n", time_eval);
 	printf("Td %.2e\n", time_drct);
-	printf("Rt %.2e\n", time_drct/time_eval);
+	printf("Rt %.2e\n", time_drct / time_eval);
 	printf("Ea %.2e\n", relerr);
 	printf("----------------------\n");
   }
