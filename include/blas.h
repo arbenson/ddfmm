88ddcfde612f56560510f225faf58bd53deed64b
Austin Benson <arbenson@gmail.com>
2013-07-23 21:10:50 -0700

    Use dscal_ and proper indentation.

--- include/blas.h
+++ include/blas.h
@@ -6,18 +6,18 @@ typedef std::complex<float> cpx8;
 typedef std::complex<double> cpx16;
 
 extern "C" {
-  void dgemm_(char *transa, char *transb, int *m, int *n, int *k, double *alpha,
-              double *a, int *lda, double *b, int *ldb, double *beta, double *c,
-              int *ldc);
-  void dgemv_(char *trans, int *m, int *n, double *alpha, double *a, int *lda,
-              double *x, int *incx, double *beta, double *y, int *incy);
-  void zgemm_(char *transa, char *transb, int *m, int *n, int *k, cpx16 *alpha,
-              cpx16 *a, int *lda, cpx16 *b, int *ldb, cpx16 *beta, cpx16 *c,
-              int *ldc); 
-  void zgemv_(char *trans, int *m, int *n, cpx16 *alpha, cpx16 *a, int *lda,
-              cpx16 *x, int *incx, cpx16 *beta, cpx16 *y, int *incy); 
+    void dgemm_(char *transa, char *transb, int *m, int *n, int *k,
+		double *alpha, double *a, int *lda, double *b, int *ldb,
+		double *beta, double *c, int *ldc);
+    void dgemv_(char *trans, int *m, int *n, double *alpha, double *a, int *lda,
+		double *x, int *incx, double *beta, double *y, int *incy);
+    
+    void zgemm_(char *transa, char *transb, int *m, int *n, int *k, cpx16 *alpha,
+		cpx16 *a, int *lda, cpx16 *b, int *ldb, cpx16 *beta, cpx16 *c,
+		int *ldc); 
+    void zgemv_(char *trans, int *m, int *n, cpx16 *alpha, cpx16 *a, int *lda,
+		cpx16 *x, int *incx, cpx16 *beta, cpx16 *y, int *incy);
+    void dscal_(int* n, double* alpha, double* X, int* incr);
 }
 
 #endif  // _BLAS_H_
-
-
