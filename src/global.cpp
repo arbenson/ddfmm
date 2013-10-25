fa8ea07f599bd614f45156f205c7f24b3bb19efe
Austin Benson <arbenson@gmail.com>
2013-10-25 13:07:52 -0700

    GPL

--- src/global.cpp
+++ src/global.cpp
@@ -1,3 +1,20 @@
+/* Distributed Directional Fast Multipole Method
+   Copyright (C) 2013 Austin Benson, Lexing Ying, and Jack Poulson
+
+ This file is part of DDFMM.
+
+    DDFMM is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DDFMM is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with DDFMM.  If not, see <http://www.gnu.org/licenses/>. */
 #ifndef RELEASE
 // If we are not in RELEASE mode, then implement wrappers for a CallStack
 #include <stack>
