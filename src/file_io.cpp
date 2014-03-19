56562b27c0230e8147f27f080d45e8a7aa1553c4
Austin Benson <arbenson@gmail.com>
2014-03-19 10:57:37 -0700

    clean up

--- src/file_io.cpp
+++ src/file_io.cpp
@@ -138,7 +138,6 @@ int LloydsAlgorithm(std::vector<int>& assignment, int num_its, int NCPU,
 	for (int i = 0; i < num_points.size(); ++i) {
 	    total_points += num_points[i];
 	}
-	std::cout << total_points << " total points." << std::endl;
 	double avg_weight = ((double) total_points) / NCPU;
 	std::vector<double> curr_weights;
 	curr_weights.insert(curr_weights.begin(), NCPU, 0);
@@ -250,9 +249,6 @@ int AssignGeom(IntNumTns& geom, std::vector<int>& num_points,
     curr_weights.insert(curr_weights.begin(), NCPU, 0);
 
     std::sort(all_weights.begin(), all_weights.end(), CompPairDescend);
-    for (int i = 0; i < all_weights.size(); ++i) {
-      std::cout << "all weights " << i << " " << all_weights[i].second << std::endl;
-    }
     for (int k = 0; k < all_weights.size(); ++k) {
 	if (all_weights[k].second == 0) {
 	    // No cells with points left to assign.
@@ -290,27 +286,23 @@ int AssignGeom(IntNumTns& geom, std::vector<int>& num_points,
 	}
     }
 
-    for (int i = 0; i < NCPU; ++i) {
+    int mpirank = getMPIRank();
+    if (mpirank == 0) {
+      for (int i = 0; i < NCPU; ++i) {
 	std::cout << "Process " << i << " has " << curr_weights[i]
                   << " cells." << std::endl;
-    }
-
-    std::cout << "Geometry-------" << std::endl << std::endl;
-    for (int k = 0; k < NC; ++k) {
-	for (int i = 0; i < NC; ++i) {
-	    std::stringstream row;
-	    for (int j = 0; j < NC; ++j) {
-		row << geom(i, j, k) << " ";
-	    }
-	    std::cout << row.str() << std::endl;
-	}
-	std::cout << std::endl;
+      }
     }
 
     return 0;
 }
 
-int NewData(std::string fname, double K, double NPW, int NCPU, int NC) {
+int NewData(std::string fname, double K, double NPW, int NCPU, int NC,
+	    IntNumTns& geom) {
+    std::cout << "K: "    << K    << std::endl
+	      << "NPW: "  << NPW  << std::endl
+	      << "NCPU: " << NCPU << std::endl
+	      << "NC: "   << NC   << std::endl;
     iA (NCPU > 0 && NPW > 0 && NC > 0 && K >= 1);
 
     std::vector<Point3> points;
@@ -339,13 +331,6 @@ int NewData(std::string fname, double K, double NPW, int NCPU, int NC) {
     std::vector<int> assignment;
 
     iC( AssignPoints(assignment, NCPU, centers, num_points, num_coords) );
-#if 0
-    for (int i = 0; i < assignment.size(); ++i) {
-	std::cout << assignment[i] << std::endl;
-    }
-#endif
-
-    // TODO: get the partition
-    IntNumTns geom(NC, NC, NC);
     iC ( AssignGeom(geom, num_points, assignment, centers, NC, NCPU, K, num_coords) );
+    return 0;
 }
